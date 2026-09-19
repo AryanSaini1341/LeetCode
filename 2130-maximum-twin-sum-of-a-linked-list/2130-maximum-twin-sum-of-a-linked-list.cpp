0fe679a4eddc8d138fc37fd19b1dd407bd0697cd
Aryan Saini <aryanofficial8380@gmail.com>
2026-09-20 00:02:57 +0530

    Time: 4 ms (56.09%), Space: 124.3 MB (65.61%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* prev = NULL;
+        ListNode* curr = slow;
+
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Compare twin pairs
+        int ans = 0;
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            ans = max(ans, first->val + second->val);
+            first = first->next;
+            second = second->next;
+        }
+
+        return ans;
+    }
+};
