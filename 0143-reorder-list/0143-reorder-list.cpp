577cc51806bee6459d7e947e2c381219b7443d9b
Aryan Saini <aryanofficial8380@gmail.com>
2026-09-21 01:02:07 +0530

    Time: 0 ms (100%), Space: 22.8 MB (51.66%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        // 1. Find middle
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // 2. Reverse second half
+        ListNode *prev = nullptr;
+        ListNode *curr = slow->next;
+        slow->next = nullptr;
+
+        while (curr) {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // 3. Merge alternately
+        ListNode *first = head;
+        ListNode *second = prev;
+
+        while (second) {
+            ListNode *t1 = first->next;
+            ListNode *t2 = second->next;
+
+            first->next = second;
+            second->next = t1;
+
+            first = t1;
+            second = t2;
+        }
+    }
+};
